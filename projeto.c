d2644d72b3d6f0f165a623da41347b43f428cb73
gureiz <gureisbee@gmail.com>
2024-09-29 23:10:20 +0000

    Base das funcoes

--- projeto.c
+++ projeto.c
@@ -1,32 +1,12 @@
 #include "projeto.h"
 #include <stdio.h>
 
-
-
-typedef struct {
-    float reais;
-    float bitcoin;
-    float ethereum;
-    float ripple;
-} Carteira;
-
-typedef struct {
-    char cpf[12];
-    char senha[20];
-    Carteira carteira;
-    int numTransacoes;
-    char historico[MAX_TRANSACOES][100];
-} Usuario;
-
 //base das funcoes
 void salvarDados(Usuario usuarios[], int numUsuarios);
 void carregarDados(Usuario usuarios[], int *numUsuarios);
 int login(Usuario usuarios[], int numUsuarios, char *cpf, char *senha);
 int cadastrarUsuario(Usuario usuarios[], int *numUsuarios);
-void menuPrincipal(Usuario *usuario){
-    printf("Menu\n");
-    return 0;
-}
+void menuPrincipal(Usuario *usuario);
 void consultarSaldo(Usuario *usuario);
 void consultarExtrato(Usuario *usuario);
 void depositarReais(Usuario *usuario);
