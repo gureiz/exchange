696b9c741f838742d8ce46845c3037d6fcbd1b99
gureiz <gureisbee@gmail.com>
2024-10-01 21:47:58 +0000

    Função de login

--- projeto.c
+++ projeto.c
@@ -1,18 +1,13 @@
 #include "projeto.h"
-#include <stdio.h>
 
-//base das funcoes
-void salvarDados(Usuario usuarios[], int numUsuarios);
-void carregarDados(Usuario usuarios[], int *numUsuarios);
-int login(Usuario usuarios[], int numUsuarios, char *cpf, char *senha);
-int cadastrarUsuario(Usuario usuarios[], int *numUsuarios);
-void menuPrincipal(Usuario *usuario);
-void consultarSaldo(Usuario *usuario);
-void consultarExtrato(Usuario *usuario);
-void depositarReais(Usuario *usuario);
-void sacarReais(Usuario *usuario);
-void comprarCripto(Usuario *usuario);
-void venderCripto(Usuario *usuario);
-void atualizarCotas();
-void registrarOperacao(Usuario *usuario, const char *operacao);
+// Função de login
+int login(Usuario usuarios[], int numUsuarios, char *cpf, char *senha) {
+  for (int i = 0; i < numUsuarios; i++) {
+    if (strcmp(usuarios[i].cpf, cpf) == 0 &&
+        strcmp(usuarios[i].senha, senha) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
 
