088cd37ef8f5743bc0636e03c722e9313c3dbf5d
cudionthemoon <130794370+cudionthemoon@users.noreply.github.com>
2024-10-01 22:12:39 +0000

    funcao de cadastramento

--- projeto.c
+++ projeto.c
@@ -1,6 +1,6 @@
 #include "projeto.h"
 
-// Função de login
+// a função de login
 int login(Usuario usuarios[], int numUsuarios, char *cpf, char *senha) {
   for (int i = 0; i < numUsuarios; i++) {
     if (strcmp(usuarios[i].cpf, cpf) == 0 &&
@@ -11,3 +11,25 @@ int login(Usuario usuarios[], int numUsuarios, char *cpf, char *senha) {
   return -1;
 }
 
+// funcao cadastro de novo usuario
+int cadastrarUsuario(Usuario usuarios[], int *numUsuarios) {
+  if (*numUsuarios >= MAX_USUARIOS) {
+    printf("Limite máximo de usuários atingido!\n");
+    return 0;
+  }
+
+  printf("Digite o CPF (11 dígitos): ");
+  scanf("%s", usuarios[*numUsuarios].cpf);
+  printf("Digite a senha: ");
+  scanf("%s", usuarios[*numUsuarios].senha);
+
+  // iniciar a carteira do usuário
+  usuarios[*numUsuarios].carteira.reais = 0.0;
+  usuarios[*numUsuarios].carteira.bitcoin = 0.0;
+  usuarios[*numUsuarios].carteira.ethereum = 0.0;
+  usuarios[*numUsuarios].carteira.ripple = 0.0;
+  usuarios[*numUsuarios].numTransacoes = 0;
+
+  (*numUsuarios)++;
+  return 1;
+}
